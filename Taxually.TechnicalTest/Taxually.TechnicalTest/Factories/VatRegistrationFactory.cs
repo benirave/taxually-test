e442c19a639813403748651b27219f227794fb84
Bence Eros <bence.eros@icloud.com>
2023-02-13 23:06:04 +0100

    Create simple factory

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Factories/VatRegistrationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Taxually.TechnicalTest.Services;
+
+namespace Taxually.TechnicalTest.Factories;
+
+public class VatRegistrationFactory : IVatRegistrationFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public VatRegistrationFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IVatRegistrationService GetVatRegistrationService(string countryCode)
+    {
+        var varRegistrationServices = _serviceProvider.GetServices<IVatRegistrationService>();
+
+        return countryCode switch
+        {
+            "GB" => varRegistrationServices.First(x => x.GetType() == typeof(UnitedKingdomVatRegistrationService)),
+            "FR" => varRegistrationServices.First(x => x.GetType() == typeof(FrenchVatRegistrationService)),
+            "DE" => varRegistrationServices.First(x => x.GetType() == typeof(GermanVatRegistrationService)),
+            _ => throw new Exception("Country not supported")
+        };
+    }
+}
