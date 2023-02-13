9e1701df73331be36b48f8decc60657fbd1d3b6b
Bence Eros <bence.eros@icloud.com>
2023-02-13 23:06:22 +0100

    Register new services + factory

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/ConfigureServices.cs
@@ -0,0 +1,18 @@
+﻿using Taxually.TechnicalTest.Factories;
+using Taxually.TechnicalTest.Services;
+
+namespace Taxually.TechnicalTest;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IVatRegistrationFactory, VatRegistrationFactory>();
+
+        services.AddTransient<IVatRegistrationService, UnitedKingdomVatRegistrationService>();
+        services.AddTransient<IVatRegistrationService, FrenchVatRegistrationService>();
+        services.AddTransient<IVatRegistrationService, GermanVatRegistrationService>();
+
+        return services;
+    }
+}
