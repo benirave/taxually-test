a0a98c48a95499a611fcc9e3223f8e3d6826f5f9
Bence Eros <bence.eros@icloud.com>
2023-02-14 00:10:18 +0100

    Added tests for VatRegistrationFactory

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.Tests/VatRegistrationFactoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Taxually.TechnicalTest.Clients;
+using Taxually.TechnicalTest.Factories;
+using Taxually.TechnicalTest.Services;
+
+namespace Taxually.TechnicalTest.Tests;
+public class VatRegistrationFactoryTests
+{
+    private IServiceCollection _serviceCollection;
+
+    public VatRegistrationFactoryTests()
+    {
+        _serviceCollection = new ServiceCollection()
+            .AddTransient<IVatRegistrationService, UnitedKingdomVatRegistrationService>()
+            .AddTransient<IVatRegistrationService, FranceVatRegistrationService>()
+            .AddTransient<IVatRegistrationService, GermanyVatRegistrationService>()
+            .AddTransient(_ => new Fake<ITaxuallyHttpClient>().FakedObject)
+            .AddTransient(_ => new Fake<ITaxuallyQueueClient>().FakedObject);
+    }
+
+    [Theory]
+    [InlineData("GB", typeof(UnitedKingdomVatRegistrationService))]
+    [InlineData("FR", typeof(FranceVatRegistrationService))]
+    [InlineData("DE", typeof(GermanyVatRegistrationService))]
+    public void GetVatRegistrationService_Returns_Correct_Implementation_Of_VatRegistrationService_By_CountryCode(string countryCode, Type serviceType)
+    {
+        using var serviceProvider = _serviceCollection.BuildServiceProvider();
+        var factory = new VatRegistrationFactory(serviceProvider);
+
+        var service = factory.GetVatRegistrationService(countryCode);
+
+        service.Should().NotBeNull();
+        service.Should().BeOfType(serviceType);
+    }
+
+    [Fact]
+    public void GetVatRegistrationService_Throws_Exception_WHen_Implementation_Of_VatRegistrationService_By_CountryCode_Is_Missing()
+    {
+        var factory = new VatRegistrationFactory(_serviceCollection.BuildServiceProvider());
+
+        var act = () => factory.GetVatRegistrationService("HU");
+
+        act.Should().ThrowExactly<Exception>().WithMessage("Country not supported");
+    }
+}
