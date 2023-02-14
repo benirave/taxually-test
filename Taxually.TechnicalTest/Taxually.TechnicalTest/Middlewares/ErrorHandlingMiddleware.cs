c84d871f2d27b335035d8ca7403b78309e15d1da
Bence Eros <bence.eros@icloud.com>
2023-02-14 02:42:37 +0100

    Simple error handling middleware to serilaze all the errors to be json

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.TechnicalTest.Middlewares;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+		try
+		{
+            await _next(context);
+        }
+        catch (Exception ex)
+		{
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsJsonAsync(new ErrorResult
+            {
+                ErrorMessage= ex.Message
+            });
+		}
+    }
+}
---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Models/ErrorResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Taxually.TechnicalTest.Models;
+
+public class ErrorResult
+{
+    public Guid TraceId { get; } = Guid.NewGuid();
+
+    public string Message => $"Please provide the following id {TraceId} to the support team.";
+
+    public string ErrorMessage { get; init; }
+}
